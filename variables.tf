22600c17fb89843c5e5cc0591b1aa3aaf5d44691
Phyo Min Htun <phyominhtun@Phyos-MacBook-Pro-2.local>
2020-11-18 22:45:50 +0630

    fix: nat gateway

--- variables.tf
+++ variables.tf
@@ -5,16 +5,19 @@ variable region {
 
 variable vpc {
   type = object({
-    cidr            = string
-    azs             = list(string)
-    pri_sub         = list(string)
-    pub_sub         = list(string)
-    is_enable_natgw = bool
+    cidr                = string
+    azs                 = list(string)
+    pri_sub             = list(string)
+    pub_sub             = list(string)
+    is_enable_natgw     = bool
+    is_enable_vpngw     = bool
+    is_single_natgw     = bool
+    is_one_natgw_per_az = bool
   })
   description = "AWS VPC Variables"
 }
 
 variable "key_name" {
-  type = string
+  type        = string
   description = "AWS Key_pair variables"
 }
