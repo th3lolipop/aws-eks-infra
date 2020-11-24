fd4a5d1ade8f3191d44de30d75d254bf132ff4c3
D Ther <dtherhtun.cw@gmail.com>
2020-11-24 13:37:40 +0630

    fix arn

--- main.tf
+++ main.tf
@@ -162,7 +162,7 @@ module "sg" {
 
 module "external-dns" {
   source  = "DTherHtun/external-dns/aws"
-  version = "0.1.7"
+  version = "0.1.8"
 
   domain           = var.domain
   k8s_cluster_name = module.eks.cluster_id
