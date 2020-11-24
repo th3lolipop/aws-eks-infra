bc49432395dd6be7b9d692d4393c9c107ad189e7
Phyo Min Htun <phyominhtun@Phyos-MacBook-Pro-2.local>
2020-11-24 22:26:00 +0630

    feat: rds instance added

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,25 @@ output "kubeconfig" {
   value       = module.eks.kubeconfig
   description = "Kubeconfig Output"
 }
+
+## AWS RDS output ##
+output "database_instance_address" {
+  value       = module.rds.this_db_instance_address
+  description = "Database Instance Address"
+}
+
+output "database_connection_endpoint" {
+  value       = module.rds.this_db_instance_endpoint
+  description = "Database Connection Endpoint"
+}
+
+output "database_username" {
+  value       = module.rds.this_db_instance_username
+  description = "Database Username"
+}
+
+output "database_password" {
+  value       = module.rds.this_db_instance_password
+  description = "Database Password"
+  sensitive   = true
+}
